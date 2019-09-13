e8eca1b1020f02a83b12d11554a1b2e946191d2b
Edgar Xolop <edgarxoix@gmail.com>
2019-09-13 00:57:41 -0600

    fix implementation

--- src/main/java/com/voider/socialapi/config/ServerConfig.java
+++ src/main/java/com/voider/socialapi/config/ServerConfig.java
@@ -2,9 +2,10 @@ package com.voider.socialapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ServerConfig {
+public class ServerConfig  implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
