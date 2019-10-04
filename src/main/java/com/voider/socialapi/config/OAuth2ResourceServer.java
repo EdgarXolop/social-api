43ef6721536851010b86bb71cdffffffcf0a1dcb
Edgar Xolop <edgarxoix@gmail.com>
2019-10-04 08:32:15 -0600

    cors config
    Support client request

--- src/main/java/com/voider/socialapi/config/OAuth2ResourceServer.java
+++ src/main/java/com/voider/socialapi/config/OAuth2ResourceServer.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
--- src/main/java/com/voider/socialapi/config/ServerConfig.java
+++ src/main/java/com/voider/socialapi/config/ServerConfig.java
@@ -1,14 +1,37 @@
 package com.voider.socialapi.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ServerConfig  implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
+//    @Bean
+//    public FilterRegistrationBean platformCorsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//
+//        CorsConfiguration configAutenticacao = new CorsConfiguration();
+//        configAutenticacao.setAllowCredentials(true);
+//        configAutenticacao.addAllowedOrigin("*");
+//        configAutenticacao.addAllowedHeader("Authorization");
+//        configAutenticacao.addAllowedHeader("Content-Type");
+//        configAutenticacao.addAllowedHeader("Accept");
+//        configAutenticacao.addAllowedMethod("POST");
+//        configAutenticacao.addAllowedMethod("GET");
+//        configAutenticacao.addAllowedMethod("DELETE");
+//        configAutenticacao.addAllowedMethod("PUT");
+//        configAutenticacao.addAllowedMethod("OPTIONS");
+//        configAutenticacao.setMaxAge(3600L);
+//        source.registerCorsConfiguration("/**", configAutenticacao);
+//
+//        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+//        bean.setOrder(-110);
+//        return bean;
+//    }
 }
